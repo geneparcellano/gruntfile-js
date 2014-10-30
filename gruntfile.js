39f10374cb2f3d8bdf473ce17592b7716d1cefca
Gene Parcellano <ginjin3@gene-air.local>
2014-10-30 15:44:38 -0500

    Add grunt-open configuration

--- gruntfile.js
+++ gruntfile.js
@@ -255,6 +255,12 @@ module.exports = function (grunt) {
 				'svgmin',
 				'htmlmin'
 			]
+		},
+		open: {
+			dev: {
+				url: 'http://localhost:<%= connect.options.port %>',
+				app: 'Google Chrome'
+			}
 		}
 	});
 
